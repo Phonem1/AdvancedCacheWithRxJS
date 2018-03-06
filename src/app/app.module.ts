7fc1d5e7fe79797f4e7e605046bb2baa2b24d1d2
Myat Thu Phone <phonemyatt.anewtech@gmail.com>
2018-03-06 10:52:05 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { JokeListComponent } from './joke-list/joke-list.component';
 
+import { APP_ROUTES } from './app.routes';
+import { JokeService } from './joke.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    JokeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
-  providers: [],
+  providers: [ JokeService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
